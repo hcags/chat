4029ff1da2dc40d25c0e807a360166a69604bb8f
hcags <120849679+hcags@users.noreply.github.com>
2024-01-16 06:07:42 -0800

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,38 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
+    alphabetKey = alphabetKey - 1
+    if (alphabetKey < 0) {
+        alphabetKey = alphabet.length - 1
+    }
+    basic.showString("" + (alphabet[alphabetKey]))
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-	
+    funkText = "" + funkText + alphabet[alphabetKey]
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-	
+    alphabetKey = alphabetKey + 1
+    if (alphabet.length <= alphabetKey) {
+        alphabetKey = 0
+    }
+    basic.showString("" + (alphabet[alphabetKey]))
 })
 radio.onReceivedString(function (receivedString) {
-	
+    basic.showString(receivedString)
 })
 input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
-	
+    basic.showString(funkText)
+    basic.showString("" + (alphabet[alphabetKey]))
 })
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
-	
+    radio.sendString(funkText)
+    alphabetKey = 0
+    basic.showString("" + (alphabet[alphabetKey]))
+    funkText = ""
 })
+let funkText = ""
 let alphabetKey = 0
+let alphabet: string[] = []
 radio.setGroup(1)
-let alphabet = [
+alphabet = [
 "A",
 "B",
 "C",
