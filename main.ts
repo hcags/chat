cf7e87923682713f1259cd7798fd270c1eec31fa
hcags <120849679+hcags@users.noreply.github.com>
2024-01-16 06:07:32 -0800

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,24 +1,38 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
+    alphabetKey = alphabetKey - 1
+    if (alphabetKey < 0) {
+        alphabetKey = alphabet.length - 1
+    }
+    basic.showString("" + (alphabet[alphabetKey]))
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-	
+    FunkText = "" + FunkText + alphabet[alphabetKey]
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-	
+    alphabetKey = alphabetKey + 1
+    if (alphabet.length <= alphabetKey) {
+        alphabetKey = 0
+    }
+    basic.showString("" + (alphabet[alphabetKey]))
 })
 radio.onReceivedString(function (receivedString) {
-	
+    basic.showString(receivedString)
 })
 input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
-	
+    basic.showString(FunkText)
+    basic.showString("" + (alphabet[alphabetKey]))
 })
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
-	
+    radio.sendString(FunkText)
+    alphabetKey = 0
+    basic.showString("" + (alphabet[alphabetKey]))
+    FunkText = ""
 })
+let FunkText = ""
 let alphabetKey = 0
+let alphabet: string[] = []
 radio.setGroup(1)
-let alphabet = [
+alphabet = [
 "A",
 "B",
 "C",
